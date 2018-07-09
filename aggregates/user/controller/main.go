8cc1e4cb0af21dfff273d7cb1495d104b6316efe
amiralii <mr.roshanae@gmail.com>
2018-07-10 01:38:24 +0430

    fixed

--- aggregates/user/controller/main.go
+++ aggregates/user/controller/main.go
@@ -1,8 +1,6 @@
 package userController
 
 import (
-	"net/http"
-
 	"github.com/amiralii/goEchoExample/config/response"
 
 	user "github.com/amiralii/goEchoExample/aggregates/user/model"
@@ -13,27 +11,26 @@ import (
 func Signup(e echo.Context) error {
 	u := new(user.User)
 	if err := e.Bind(u); err != nil {
-		return err
+		return response.Error(err.Error(), 500)
 	}
 	if err := e.Validate(u); err != nil {
 		return response.Error(err.Error(), 422)
 	}
 	err := Repo.Signup(*u)
 	if err != nil {
-		return err
+		return response.Error(err.Error(), 500)
 	}
 	return response.Created(e, "user created")
 }
 
 func Signin(e echo.Context) error {
 	u := new(user.User)
-	e.Bind(u)
-	if u.Username == "" || u.Password == "" {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: "invalid username or password"}
+	if err := e.Bind(u); err != nil {
+		return response.Error(err.Error(), 500)
 	}
 	data, err := Repo.FindUser(u.Username, u.Password)
 	if err != nil {
-		return err
+		return response.Error(err.Error(), 500)
 	}
 	return response.Ok(e, data)
 }
